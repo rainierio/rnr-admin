2fb6ccaa463bd313798daf93988983c2580e0844
rainierio <rainieriletidjawa@gmail.com>
2020-09-26 20:30:08 +1000

    ok

--- src/pages/rainierio/BlogPage/BlogAddPage.js
+++ src/pages/rainierio/BlogPage/BlogAddPage.js
@@ -125,6 +125,28 @@ class BlogAddPage extends React.Component {
                         apiKey="8ubcryf7abla9utxbii92w2drh8hz8rjn0aes5216kek6h77"
                         value={addPost.content}
                         init={{
+                          file_picker_callback: function (
+                            callback,
+                            value,
+                            meta,
+                          ) {
+                            // Provide file and text for the link dialog
+                            if (meta.filetype == 'file') {
+                              callback('mypage.html', { text: 'My text' });
+                            }
+                            // Provide image and alt text for the image dialog
+                            if (meta.filetype == 'image') {
+                              callback('myimage.jpg', { alt: 'My alt text' });
+                            }
+
+                            // Provide alternative source and posted for the media dialog
+                            if (meta.filetype == 'media') {
+                              callback('movie.mp4', {
+                                source2: 'alt.ogg',
+                                poster: 'image.jpg',
+                              });
+                            }
+                          },
                           height: 500,
                           menubar: true,
                           plugins: [
